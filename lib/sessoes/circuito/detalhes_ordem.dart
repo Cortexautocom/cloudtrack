e8e339eee6d7537cb9f74292d628c9f0035f5a17
Leonardo <cortexautocom@gmail.com>
2026-02-05 14:46:19 -0300

    Alteração comum

--- lib/sessoes/circuito/detalhes_ordem.dart
+++ lib/sessoes/circuito/detalhes_ordem.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import '../apuracao/certificado_apuracao.dart'; // Import corrigido
+import '../apuracao/certificado_apuracao_saida.dart'; // Import corrigido
 
 enum EtapaCircuito {
   programado,
